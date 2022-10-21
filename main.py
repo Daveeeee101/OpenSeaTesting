486e58781bc8d8e737220cc2d824e5fbc52978c6
David <DavidBurkett02@gmail.com>
2022-10-21 22:59:43 +0100

    testing

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from datetime import datetime, timedelta
+
 import GraphQLData
 import requests
 
 graphURL = "https://api.opensea.io/graphql/"
 
 if __name__ == '__main__':
-    header = GraphQLData.getHeader('EventHistoryPollQuery')
-    body = GraphQLData.getQuery('EventHistoryPollQuery', archetype=None,
-                                collections=['dodoor-nft'], chains=[],
-                                eventTypes=['AUCTION_CREATED', 'AUCTION_SUCCESSFUL', 'AUCTION_CANCELLED'])
+    header = GraphQLData.getHeader('OrdersQuery')
+    body = GraphQLData.getQuery('OrdersQuery', )
     with requests.session() as s:
         rep = requests.post(url=graphURL, headers=header, data=body)
         print(rep.text)
