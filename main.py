d5d362d9cd67b83e02da3381d1ad81dd2d7cffb8
David <DavidBurkett02@gmail.com>
2022-10-21 23:07:09 +0100

    testing

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ graphURL = "https://api.opensea.io/graphql/"
 
 if __name__ == '__main__':
     header = GraphQLData.getHeader('OrdersQuery')
-    body = GraphQLData.getQuery('OrdersQuery', )
+    body = GraphQLData.getQuery('OrdersQuery', makerArchetype={'assetContractAddress': "0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258", 'tokenId': "65268", 'chain': "ETHEREUM"}, isValid=True)
     with requests.session() as s:
         rep = requests.post(url=graphURL, headers=header, data=body)
         print(rep.text)
